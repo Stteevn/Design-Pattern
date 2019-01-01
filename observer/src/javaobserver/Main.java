97c5f0cb6b52853b14fcc7a8dc3a93accf5914e4
stvn <stteevntang@gmail.com>
2019-01-01 16:28:56 +0800

    commit

--- observer/src/javaobserver/Main.java
+++ observer/src/javaobserver/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         WeatherData weatherData = new WeatherData();
         GeneralDisplay generalDisplay = new GeneralDisplay(weatherData);
+        StupidDisplay stupidDisplay = new StupidDisplay(weatherData);
         weatherData.measurementChanged(10, 65, 124);
     }
 }
